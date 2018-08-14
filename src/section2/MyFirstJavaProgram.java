a9de4fefe80d9e16c2a699f9939a4724b3936191
iqh2007 <league@WTS21.local>
2018-08-14 12:00:53 -0700

    spiral

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot r2d2 = new Robot();
+	r2d2.setSpeed(100);
 	
+	r2d2.setPenWidth(5);
+	
+	r2d2.setPenColor(Color.CYAN);
+	
+	r2d2.penDown();
+
+	for (int i=0; i<9; i++) {
+		r2d2.move(100);
+		r2d2.turn(-90);
+		r2d2.setPenColor(Color.magenta);
+		r2d2.setPenColor(Color.pink);
+		r2d2.move(50);
+		r2d2.setPenColor(Color.GREEN);
+		r2d2.move(50);
+		r2d2.setPenColor(Color.BLUE);
+
 		
+		
+	}
 		
 	}
 }
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,17 +10,18 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
+		Robot r2d2 = new Robot();
 
 		// 3. Put the robot's pen down
-
+		r2d2.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+		r2d2.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+		r2d2.setPenColor(Color.red);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+		
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+		
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
 
 			// 2. Move the robot 200 pixels
