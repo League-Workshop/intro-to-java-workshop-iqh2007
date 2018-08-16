3df19dc4b71aa77ac22a6d2fd75d14e74fdeb81f
AnkitBisain <league@WTS21.attlocal.net>
2018-08-16 12:02:22 -0700

    pentagon crazy

--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -1,35 +1,44 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+	Robot r2d2 = new Robot(800,550);
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
-
+		
 		// 6. Make the robot go as fast as possible
-
+		r2d2.setSpeed(100);
+		r2d2.penDown();
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+		int length = 50;
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
-
+		for(int i=0; i<60;) {
 			// 9. Change the color of the pen to a random color
-	
+			r2d2.setPenColor( 170, 221,255);
 			// 8. Increase the length variable by 10
-	
+			length = length + 10;
 			// 5. call your drawTriangle() method using your length variable
-	
+			drawTriangle(length);
 			// 10. Turn the robot 6 degrees to the right
-
+			r2d2.turn(6);
+		}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
+	
 	private void drawTriangle(int length) {
 		
+	for(int i=0; i<3; i++) {
+	
+		r2d2.move(length);
+		r2d2.turn(360/3);
 		
+	}
 		
 	}
 
--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,13 +9,18 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+		speak(" Spell conscience");
 		// 2. Catch the user's answer in a String
-
+		String answer = JOptionPane.showInputDialog("Please spell the word.");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+		if (answer.equals("conscience")){
+		JOptionPane.showMessageDialog(null, "correct!");
+		}
+		
 		// 4. Otherwise say "wrong"
-
+		else {
+		JOptionPane.showMessageDialog(null, "incorrect!");
+		}
 		// 5. repeat the process for other words
 		
 	}
