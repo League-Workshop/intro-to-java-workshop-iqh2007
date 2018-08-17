5cc5905202bfc68fc0d61bf6ce9b36db6f9c90fb
iqh2007 <league@WTS21.attlocal.net>
2018-08-17 12:00:25 -0700

    scary maze

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -15,7 +15,7 @@ public class PentagonCrazy {
 		// 3. Put the robot's pen down
 		r2d2.penDown();
 		// 8. Make the robot go at maximum speed (100)
-		r2d2.setSpeed(100);
+		r2d2.setSpeed(500);
 		// 9. Set the pen to a color that you like for the shape
 		r2d2.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
@@ -23,7 +23,7 @@ public class PentagonCrazy {
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
 		int angle = 360/5;
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
-		for(int i=0; i<200; i++) {
+		for(int i=0; i<500; i++) {
 			// 2. Move the robot 200 pixels
 		
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
